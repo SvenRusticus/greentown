c60243dd9a5ae6d66b792d52ead76e9470c42858
Stephen van Essen <stephen@icemedia.nl>
2020-03-08 22:00:07 +0100

    Add project files.

---
+++ 13041_GreenTown/Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using MVCUmbraco.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace MVCUmbraco.Controllers
+{
+    public class ContactController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [HttpPost]
+        public ActionResult SendMail(Contact form)
+        {
+            string retValue = "There was an error submitting the form, please try again later.";
+            if (!ModelState.IsValid)
+            {
+                return Content(retValue);
+            }
+
+            if (ModelState.IsValid)
+            {
+                //Update your SMTP server credentials
+                using (var client = new SmtpClient
+                {
+                    //Host = "10.31.10.11",
+                    Host = ConfigurationManager.AppSettings["SMTP_Server"],
+
+                    //Port = 587,
+                    //EnableSsl = true,
+                    //Credentials = new NetworkCredential("account@gmail.com", "password"),
+                    DeliveryMethod = SmtpDeliveryMethod.Network
+                })
+                {
+                    var mail = new MailMessage();
+                    var receivers = ConfigurationManager.AppSettings["Contact_To_Address"].Split(';');
+                    foreach (var receiver in receivers)
+                    {
+                        mail.To.Add(receiver); // Update your email address
+                    }
+                   
+                    mail.From = new MailAddress(form.Email, form.Name);
+                    mail.Subject = String.Format("Request to Contact from");
+                    mail.Body = form.Message;
+                    mail.IsBodyHtml = false;
+                    try
+                    {
+                        client.Send(mail);
+                        retValue = "Your Request for Contact was submitted successfully.<br/>We will contact you shortly.";
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+            return Content(retValue);
+        }
+    }
+}
---
+++ 13041_GreenTown/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCUmbraco.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+       
+        [Required]
+        public string Message { get; set; }
+    }
+}
